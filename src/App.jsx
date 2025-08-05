921063057df136b2d87fd6447fd80f92be8425b6
d4v1dm7rs <ds093461@gmail.com>
2025-08-04 21:19:36 -0300

    Aprendendo react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import HelloWorld from './components/helloWorld'
+import Frase from './components/Frase'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Meu Primeiro Componente HelloWorld</h1>
+      <HelloWorld/>
+      <Frase/>
+    </div>
+  )
+}
+export default App
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+function HelloWorld () {
+    
+    return(
+        <div>
+            <p>Meu Primeiro componente</p>
+        </div>
+    )
+}
+export default HelloWorld
